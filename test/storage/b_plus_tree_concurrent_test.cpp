2e16d05b7d85d6c849ddf44a2fbdb7cfa6444b29
dongdongyang <duomipopo@qq.com>
2021-08-30 10:25:55 +0000

    customized test

--- test/storage/b_plus_tree_concurrent_test.cpp
+++ test/storage/b_plus_tree_concurrent_test.cpp
@@ -300,7 +300,7 @@ TEST(BPlusTreeConcurrentTest, DISABLED_DeleteTest2) {
   remove("test.log");
 }
 
-TEST(BPlusTreeConcurrentTest, MixTest) {
+TEST(BPlusTreeConcurrentTest, DISABLED_MixTest) {
   // create KeyComparator and index schema
   Schema *key_schema = ParseCreateStatement("a bigint");
   GenericComparator<8> comparator(key_schema);
@@ -347,5 +347,234 @@ TEST(BPlusTreeConcurrentTest, MixTest) {
   remove("test.db");
   remove("test.log");
 }
+
+
+void InsertDeleteOne(BPlusTree<GenericKey<8>, RID, GenericComparator<8>> &tree,
+	bool* ret, uint64_t thread_itr) {
+	ret[thread_itr] = true;
+	GenericKey<8> index_key;
+	RID rid;
+	std::vector<RID> values;
+	// create transaction
+	Transaction *transaction = new Transaction(0);
+  Transaction *transaction_for_read = new Transaction(-1);
+	for (int i = 0; i < 2000; ++i) {
+		int64_t value = (int64_t)thread_itr;
+		rid.Set((int32_t)(value), value);
+		index_key.SetFromInteger(value);
+    
+		if (!tree.Insert(index_key, rid, transaction))
+		{
+			ret[thread_itr] = false;
+			break;
+		}
+		if (!tree.GetValue(index_key, &values,transaction_for_read))
+		{
+			ret[thread_itr] = false;
+			break;
+		}
+		tree.Remove(index_key, transaction);
+		if (tree.GetValue(index_key, &values,transaction_for_read))
+		{
+			ret[thread_itr] = false;
+			break;
+		}
+	}
+	delete transaction;
+  delete transaction_for_read;
+}
+
+TEST(BPlusTreeConcurrentTest, DISABLED_RootLatchTest) {
+	// create KeyComparator and index schema
+	Schema *key_schema = ParseCreateStatement("a bigint");
+	GenericComparator<8> comparator(key_schema);
+
+	DiskManager *disk_manager = new DiskManager("test.db");
+	BufferPoolManager *bpm = new BufferPoolManager(5, disk_manager);
+	// create b+ tree
+	BPlusTree<GenericKey<8>, RID, GenericComparator<8>> tree("foo_pk", bpm, comparator);
+	//GenericKey<8> index_key;
+	RID rid;
+
+	// create and fetch header_page
+	page_id_t page_id;
+	auto header_page = bpm->NewPage(&page_id);
+	(void)header_page;
+	
+	int times = 3;
+	bool* ans = new bool[times];
+	LaunchParallelTest(times, InsertDeleteOne, std::ref(tree), ans);
+
+	for (int i = 0; i < times; ++i)
+	{
+		EXPECT_EQ(ans[i], true);
+	}
+	delete[] ans;
+
+	EXPECT_EQ(tree.IsEmpty(), true);
+
+	bpm->UnpinPage(HEADER_PAGE_ID, true);
+	delete disk_manager;
+	delete bpm;
+	remove("test.db");
+	remove("test.log");
+
+  delete key_schema;
+}
+
+int64_t GetSetRandomValue(std::set<int64_t>& value_sets)
+{
+	assert(!value_sets.empty());
+	int pos = rand() % value_sets.size();
+	for (auto i = value_sets.begin(); i != value_sets.end(); ++i)
+	{
+		if (pos == 0) {
+			return *i;
+		}
+		--pos;
+	}
+	assert(false);
+}
+
+void InsertDeleteRandom(BPlusTree<GenericKey<8>, RID, GenericComparator<8>> &tree,
+	std::set<int64_t>* value_sets, uint64_t thread_itr) {
+  // get tid 
+  std::thread::id thread_id = std::this_thread::get_id();
+  uint32_t* tidp = reinterpret_cast<uint32_t*>(&thread_id);
+  uint32_t tid = (*tidp)%10000;
+
+
+	std::set<int64_t>& value_set = value_sets[thread_itr];
+	std::set<int64_t> delete_set;
+	GenericKey<8> index_key;
+	RID rid;
+	std::vector<RID> values;
+	// create transaction
+	Transaction *transaction = new Transaction(0);
+	for (int i = 0; i < 1000; ++i) {
+		bool isDeleted;
+		int64_t value;
+		if (value_set.empty()){
+        isDeleted = false;
+        if (delete_set.empty())	{
+            value = (int64_t)(1000 * thread_itr + rand() % 200);
+        }	else {
+            value = GetSetRandomValue(delete_set);
+        }
+		}	else {
+        isDeleted = rand() % 100 < 40;
+        if (isDeleted) {
+            value = GetSetRandomValue(value_set);
+        }	else{
+          if (delete_set.empty())	{
+              value = (int64_t)(1000 * thread_itr + rand() % 200);
+              if (value_set.find(value) != value_set.end())	{
+                  continue;
+              }
+          }	else {
+              value = GetSetRandomValue(delete_set);
+          }
+        }
+		}
+		rid.Set((int32_t)(value), value);
+		index_key.SetFromInteger(value);
+		if (isDeleted) {
+        bool isSuccess = tree.Remove(index_key, transaction);
+        value_set.erase(value);
+        delete_set.insert(value);
+        if (!isSuccess) {
+           LOG_INFO("[%u-InsertDeleteRandomf] test delete %ld failed.", tid, value);
+        } else {
+            LOG_INFO("[%u-InsertDeleteRandom] test delete %ld success.", tid, value);
+        }
+		}	else {
+        bool isSuccess = tree.Insert(index_key, rid, transaction);
+        value_set.insert(value);
+        delete_set.erase(value);
+        if (!isSuccess) {
+           LOG_INFO("[%u-InsertDeleteRandomf] test insert %ld failed.", tid, value);
+        } else {
+           LOG_INFO("[%u-InsertDeleteRandom] test insert %ld success.", tid, value);
+        }
+		}
+	}
+	delete transaction;
+}
+
+TEST(BPlusTreeConcurrentTest, RandomTest) {
+	// create KeyComparator and index schema
+	Schema *key_schema = ParseCreateStatement("a bigint");
+	GenericComparator<8> comparator(key_schema);
+
+	DiskManager *disk_manager = new DiskManager("test.db");
+	BufferPoolManager *bpm = new BufferPoolManager(20, disk_manager);
+	// create b+ tree
+	BPlusTree<GenericKey<8>, RID, GenericComparator<8>> tree("foo_pk", bpm, comparator, 15, 15);
+	//GenericKey<8> index_key;
+	RID rid;
+
+	// create and fetch header_page
+	page_id_t page_id;
+	auto header_page = bpm->NewPage(&page_id);
+	(void)header_page;
+
+	int times = 4;
+	std::set<int64_t>* value_sets = new std::set<int64_t>[times];
+	LaunchParallelTest(times, InsertDeleteRandom, std::ref(tree), value_sets);
+	std::set<int64_t> all_values;
+	for (int i = 0; i < times; ++i){
+		for (auto j = value_sets[i].begin(); j != value_sets[i].end(); ++j){
+			all_values.insert(*j);
+		}
+	}
+	delete[] value_sets;
+
+
+  uint64_t size = 0;
+  uint64_t valuesize = all_values.size();
+  uint64_t pre;
+  for (auto it = tree.begin(); it != tree.End(); ++it) {
+    uint64_t key = (*it).first.ToString();
+    if (size != 0) {
+      LOG_INFO("[concurrentTest-random] <pre, current> = <%lu, %lu>", pre, key);
+      EXPECT_EQ(pre < key, true);
+    }
+    pre = key;
+    auto sit = all_values.find(key);
+    EXPECT_EQ(sit != all_values.end(), true);
+    if(sit != all_values.end()) all_values.erase(sit);
+    size += 1;
+  }
+  EXPECT_EQ(size, valuesize); 
+
+  if(!all_values.empty()){
+      LOG_INFO("[concurrentTest-random] left value in value set:");
+      for (auto it = all_values.begin(); it != all_values.end(); it++){
+          uint64_t key = *it;
+          LOG_INFO("%lu", key);
+      }
+  }
+
+
+
+	//printf("value set size:%lu\n", all_values.size());
+  /*
+	uint64_t size = 0;
+	for (auto iterator = tree.begin(); iterator != tree.End(); ++iterator) {
+		int64_t key  = (*iterator).first.ToString();
+		EXPECT_EQ(all_values.find(key) != all_values.end(), true);
+		size = size + 1;
+	}
+	EXPECT_EQ(size, all_values.size()); */
+
+	bpm->UnpinPage(HEADER_PAGE_ID, true);
+	delete disk_manager;
+	delete bpm;
+	remove("test.db");
+	remove("test.log");
+
+  delete key_schema;
+}
+
 
 }  // namespace bustub
